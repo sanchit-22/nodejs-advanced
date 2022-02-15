03d14e236abf9420fe802fb90678ccbd7567f509
sanchit-22 <kumarsanchit1192081@gmailc.om>
2022-02-15 13:33:41 +0530

    Basic of NodeJs

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("welcome to the world of nodejs")
+
+function Sample(){
+    return "this is a sample fucntion"
+}
+console.log(Sample())
+
+const names=['Scott','adam','Titan']
+console.log(names)
+
+var obj={
+    name : "scott"
+}
+console.log(obj)
+
+console.log("I am using nodemon")
